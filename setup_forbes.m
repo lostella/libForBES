07a0457f0d5305643c86f9ce383f50b58d4db4bd
Lorenzo Stella <lorenzostella@gmail.com>
2015-05-05 15:43:17 +0200

    updated setup script

--- setup_forbes.m
+++ setup_forbes.m
@@ -1,10 +1,12 @@
-% Compile necessary C source files
-mex -outdir private/ private/LBFGS.c
-mex -outdir tests/ tests/RiccatiSolve.c
-
 % Add ForBES directory to MATLAB's path
-this_path = mfilename('fullpath');
-split_path = strsplit(this_path, '/');
-forbes_path = strjoin(split_path(:,1:end-1), '/');
+forbes_path = fileparts(mfilename('fullpath'));
 addpath(forbes_path);
 savepath;
+
+% Compile necessary C source files
+private_path = fullfile(forbes_path, 'private');
+tests_path = fullfile(forbes_path, 'tests');
+LBFGS_path = fullfile(forbes_path, 'private', 'LBFGS.c');
+Riccati_path = fullfile(forbes_path, 'tests', 'RiccatiSolve.c');
+mex('-outdir', private_path, LBFGS_path);
+mex('-outdir', tests_path, Riccati_path);
