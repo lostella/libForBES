b83ab14456127ace8bcee718ab193edd7bca81af
Lorenzo Stella <lorenzostella@gmail.com>
2015-05-05 15:57:03 +0200

    updated setup script

--- setup_forbes.m
+++ setup_forbes.m
@@ -1,5 +1,6 @@
 % Add ForBES directory to MATLAB's path
 forbes_path = fileparts(mfilename('fullpath'));
+display(['Adding ForBES directory to MATLAB path: ', forbes_path]);
 addpath(forbes_path);
 savepath;
 
@@ -8,5 +9,7 @@ private_path = fullfile(forbes_path, 'private');
 tests_path = fullfile(forbes_path, 'tests');
 LBFGS_path = fullfile(forbes_path, 'private', 'LBFGS.c');
 Riccati_path = fullfile(forbes_path, 'tests', 'RiccatiSolve.c');
-mex('-outdir', private_path, LBFGS_path);
-mex('-outdir', tests_path, Riccati_path);
+error_msg = 'The C compiler could not succesfully compile ';
+if mex('-outdir', private_path, LBFGS_path), error([error_msg, LBFGS_path]); end
+if mex('-outdir', tests_path, Riccati_path), error([error_msg, Riccati_path]); end
+display('ForBES was succesfully configured and installed');
