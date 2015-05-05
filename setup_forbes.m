28f27239996e78a605ebf369bf961598fbbe3b56
Lorenzo Stella <lorenzostella@gmail.com>
2015-05-05 14:28:34 +0200

    updated setup script

--- setup_forbes.m
+++ setup_forbes.m
@@ -5,6 +5,6 @@ mex -outdir tests/ tests/RiccatiSolve.c
 % Add ForBES directory to MATLAB's path
 this_path = mfilename('fullpath');
 split_path = strsplit(this_path, '/');
-forbes_path = split_path(:,1:end-1);
+forbes_path = strjoin(split_path(:,1:end-1), '/');
 addpath(forbes_path);
 savepath;
