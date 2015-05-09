515d62928f53fb00229155414348c4f0421c5084
panpat <panospatrinos@gmail.com>
2015-05-09 17:51:53 +0200

    updated squaredNorm

--- library/squaredNorm.m
+++ library/squaredNorm.m
@@ -1,12 +1,31 @@
-function obj = squaredNorm(lam)
+function obj = squaredNorm(lam,p)
+% constructs (lam/2)||x-p||^2 and its conjugate
+% arguments lam p are optional. if omitted then
+% lam = 1 and p=0;
     if nargin < 1
         lam = 1;
     end
-    obj.makef = @() @(x) call_squaredNorm(x, lam);
-    obj.makefconj = @() @(x) call_squaredNorm(x, 1/lam);
+    if nargin==2 && ~isempty(p)
+        obj.makef = @() @(x) call_squaredDist(x, lam, p);
+        obj.makefconj = @() @(x) call_squaredDist_conj(x, 1/lam, p);
+    else
+        obj.makef = @() @(x) call_squaredNorm(x, lam);
+        obj.makefconj = @() @(x) call_squaredNorm(x, 1/lam);
+    end
 end
 
 function [val, grad] = call_squaredNorm(x, lam)
     grad = lam*x;
-    val = 0.5*lam*(grad'*x);
+    val = 0.5*(grad'*x);
+end
+
+function [val, grad] = call_squaredDist(x, lam, p)
+    xp = x - p;
+    grad = lam*xp;
+    val = 0.5*(grad'*xp);
+end
+
+function [val, grad] = call_squaredDist_conj(y, lam, p)
+    grad = p + lam*y;
+    val =0.5*(y'*(grad + p));
 end
