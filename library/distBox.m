de64245e8a0620d0d6f961cb8c37477756fab2b6
panpat <panospatrinos@gmail.com>
2015-05-10 21:36:59 +0200

    fixed the problem with inf values of weights

--- library/distBox.m
+++ library/distBox.m
@@ -7,5 +7,8 @@ end
 function [prox, val] = call_distBox_prox(x, gam, lb, ub, weights)
 mu = gam*weights;
 prox = max(x - ub - mu,0) - max(lb - x - mu,0) + min(max(x,lb),ub);
-val = sum(weights.*abs(prox-min(max(prox,lb),ub)));
+if nargout>1
+    finw = ~isinf(weights);    
+    val = sum(weights(finw).*abs(prox(finw)-min(max(prox(finw),lb(finw)),ub(finw))));
+end
 end
