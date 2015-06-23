8e2c1d99ee6111835003a97f72926a24d4ce76dd
Lorenzo Stella <lorenzostella@gmail.com>
2015-06-23 14:50:53 +0200

    fixed support for linear operators

--- forbes.m
+++ forbes.m
@@ -163,14 +163,26 @@ function prob = MakeProb(fs, gs, init, aff, constr)
                 if isfield(fs{i}, 'isQuadratic') && fs{i}.isQuadratic
                     prob.f1 = fs{i};
                     if flagaff
-                        prob.C1 = horzcat(aff{i,1:N});
+                        op = separableSumLinear(aff(i,1:N));
+                        if isstruct(op)
+                            prob.C1 = op.makeop();
+                            prob.C1t = op.makeadj();
+                        else
+                            prob.C1 = op;
+                        end
                         if flagd, prob.d1 = -aff{i,N+1}; end
                     end
                 end
                 if ~isfield(fs{i}, 'isQuadratic') || ~fs{i}.isQuadratic
                     prob.f2 = fs{i};
                     if flagaff
-                        prob.C2 = horzcat(aff{i,1:N});
+                        op = separableSumLinear(aff(i,1:N));
+                        if isstruct(op)
+                            prob.C2 = op.makeop();
+                            prob.C2t = op.makeadj();
+                        else
+                            prob.C2 = op;
+                        end
                         if flagd, prob.d2 = -aff{i,N+1}; end
                     end
                 end
@@ -213,5 +225,59 @@ function prob = MakeProb(fs, gs, init, aff, constr)
             end
             prob.B = horzcat(constr{M+1:M+N});
             prob.b = constr{M+N+1};
+    end
+end
+
+function op = separableSumLinear(linops)
+    N = length(linops);
+    % for a single linear operator, return the operator itself
+    if N == 1
+        if isa(linops, 'cell') op = linops{1};
+        else op = linops; end
+        return;
+    end
+    flagstruct = zeros(1,N);
+    n = 0;
+    for i = 1:N
+        if isstruct(linops{i})
+            flagstruct(i) = 1;
+            m = linops{i}.m;
+            n = n + linops{i}.n;
+            linops{i}.op = linops{i}.makeop();
+            linops{i}.adj = linops{i}.makeadj();
+        else
+            m = size(linops{i},1);
+            n = n + size(linops{i},2);
+        end
+    end
+    % if all operators are matrices, simply stack them horizontally
+    if flagstruct == 0, op = horzcat(linops{:}); return; end
+    % otherwise make a structure
+    op.m = m;
+    op.n = n;
+    op.makeop = @() @(x) call_separableSumLinear(x, linops, N, m, flagstruct);
+    op.makeadj = @() @(y) call_separableSumLinear_adj(y, linops, N, flagstruct);
+end
+
+function y = call_separableSumLinear(x, linops, N, m, flags)
+    y = zeros(m,1);
+    k = 0;
+    for i=1:N
+        if flags(i)
+            y = y + linops{i}.op(x(k+1:k+linops{i}.n));
+        else
+            y = y + linops{i}*x(k+1:k+linops{i}.n);
+        end
+    end
+end
+
+function x = call_separableSumLinear_adj(y, linops, N, flags)
+    x = [];
+    for i=1:N
+        if flags(i)
+            x = [x; linops{i}.adj(y)];
+        else
+            x = [x; linops{i}*y];
+        end
     end
 end
