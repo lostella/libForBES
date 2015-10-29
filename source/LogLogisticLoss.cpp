36fc6091735e5089a35525f340628acd68c946d1
P. Sopasakis <p.sopasakis@gmail.com>
2015-10-29 17:45:16 +0100

    Nightly implementation of Log-logistic loss

---
+++ source/LogLogisticLoss.cpp
@@ -0,0 +1,63 @@
+/* 
+ * File:   LogLogisticLoss.cpp
+ * Author: Pantelis Sopasakis
+ * 
+ * Created on October 29, 2015, 5:08 PM
+ */
+
+#include "LogLogisticLoss.h"
+
+LogLogisticLoss::LogLogisticLoss() {
+    mu = 1.0;
+}
+
+LogLogisticLoss::~LogLogisticLoss() {
+}
+
+LogLogisticLoss::LogLogisticLoss(double mu) :
+Function(), mu(mu) {
+}
+
+int LogLogisticLoss::call(Matrix& x, double& f, Matrix& grad) {
+    //LCOV_EXCL_START
+    if (!x.isColumnVector()) {
+        throw std::invalid_argument("x must be a column-vector");
+    }
+    //LCOV_EXCL_STOP
+    f = 0.0;
+    double si = 0.0;
+    for (size_t i = 0; i < x.getNrows(); i++) {
+        si = std::exp(x.get(i, 0));
+        si = si / (1 + si);
+        f += std::log(si);
+        grad.set(i, 0, mu * (si - 1));
+    }
+    f *= mu;
+    return ForBESUtils::STATUS_OK;
+}
+
+int LogLogisticLoss::call(Matrix& x, double& f) {
+    //LCOV_EXCL_START
+    if (!x.isColumnVector()) {
+        throw std::invalid_argument("x must be a column-vector");
+    }
+    //LCOV_EXCL_STOP
+    f = 0.0;
+    double si = 0.0;
+    for (size_t i = 0; i < x.getNrows(); i++) {
+        si = std::exp(x.get(i, 0));
+        si = si / (1 + si);
+        f += std::log(si);
+    }
+    f *= mu;
+    return ForBESUtils::STATUS_OK;
+}
+
+FunctionOntologicalClass LogLogisticLoss::category() {
+    return FunctionOntologyRegistry::loss();
+}
+
+
+
+
+
---
+++ source/LogLogisticLoss.h
@@ -0,0 +1,77 @@
+/* 
+ * File:   LogLogisticLoss.h
+ * Author: Pantelis Sopasakis
+ *
+ * Created on October 29, 2015, 5:08 PM
+ * 
+ * ForBES is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * ForBES is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ForBES. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LOGLOGISTICLOSS_H
+#define	LOGLOGISTICLOSS_H
+
+#include "Function.h"
+
+/**
+ * \class LogLogisticLoss
+ * \brief Log-logistic loss function
+ * \version version 0.1
+ * \ingroup Functions
+ * \date Created on October 29, 2015, 5:08 PM
+ * \author Pantelis Sopasakis
+ * 
+ * This class extends the class Function and implements the log-logistic loss
+ * function which is a function \f$f:\mathbb{R}^n\to\mathbb{R}\f$ defined as 
+ * 
+ * \f[
+ *  f(x) = \mu \sum_{i=1}^{n} \ln \sigma(x_i),
+ * \f]
+ * 
+ * where
+ * 
+ * \f[
+ *  \sigma(z) = \frac{e^{z}}{1+e^{z}}.
+ * \f]
+ * 
+ * The gradient of \f$f\f$, \f$\nabla f:\mathbb{R}^n \to \mathbb{R}^n \f$ is computed element-wise by
+ * 
+ * \f[
+ * \nabla f(x)_i = \mu ( \sigma(x_i) - 1 ).
+ * \f]
+ * 
+ */
+
+class LogLogisticLoss : public Function {
+public:
+    LogLogisticLoss(); // with mu = 1    
+    
+    LogLogisticLoss(double mu);
+
+    virtual ~LogLogisticLoss();
+    
+    virtual int call(Matrix& x, double& f, Matrix& grad);
+
+    virtual int call(Matrix& x, double& f);
+
+    virtual FunctionOntologicalClass category();
+
+
+
+private:
+    double mu;
+
+};
+
+#endif	/* LOGLOGISTICLOSS_H */
+
