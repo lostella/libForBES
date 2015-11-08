a79d2402100a986bcf881542457d679319338c02
Lorenzo Stella <lorenzostella@gmail.com>
2015-11-08 11:52:03 +0100

    oops, forgot one file

--- source/FBProblem.h
+++ source/FBProblem.h
@@ -22,14 +22,14 @@
  */
 class FBProblem {
 private:
-    Function * m_f1;
-    Function * m_f2;
-    Function * m_g;
-    LinearOperator * m_L1;
-    LinearOperator * m_L2;
-    Matrix * m_d1;
-    Matrix * m_d2;
-    Matrix * m_lin;
+    Function * m_f1 = NULL;
+    Function * m_f2 = NULL;
+    Function * m_g = NULL;
+    LinearOperator * m_L1 = NULL;
+    LinearOperator * m_L2 = NULL;
+    Matrix * m_d1 = NULL;
+    Matrix * m_d2 = NULL;
+    Matrix * m_lin = NULL;
 
 public:
 
@@ -41,6 +41,16 @@ public:
             LinearOperator& L_2,
             Matrix& d_2,
             Matrix& linear,
+            Function& fun_g);
+    
+    FBProblem(
+            Function& fun_f1,
+            LinearOperator& L_1,
+            Matrix& d_1,
+            Function& fun_g);
+            
+    FBProblem(
+            Function& fun_f1,
             Function& fun_g);
 
     Function * f1();
